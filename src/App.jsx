46fb31b9e9ffaed264a001d4428275c595d3f146
Kenneth Camacho <Kennethcxv@gmail.com>
2026-02-04 14:26:00 -0500

    Added all the necessary installations

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+        <p className="mt-3 max-w-xl text-blue-900">
+          If you see the dark background, big headline, and styled buttons/cards, Tailwind is working.
+        </p>
+        <Button variant="contained">Text</Button>
+    </>
+  )
+}
+
+export default App
