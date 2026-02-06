d7018c8b24a234601e7d681ad461a783f7de71ef
Kenneth Camacho <Kennethcxv@gmail.com>
2026-02-06 16:16:18 -0500

    Progress on NavBar UI

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,70 @@
+import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import ToggleOffOutlinedIcon from "@mui/icons-material/ToggleOffOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import HubSpotLogo from "./assets/HubSpotLogo.png";
 
 const NavBar = () => {
-    return(
-        <>
-            <h1>Hello this is the NavBar</h1>
-        </>
-    )
-}
-export default NavBar
+  const utilityButtonSx = {
+    fontSize: 10,
+    fontWeight: 800,
+  };
+
+  
+  return (
+    <AppBar
+      position="static"
+      sx={{ bgcolor: "background.paper", color: "text.secondary" }}
+    >
+      <Toolbar sx={{ mx: 20 }}>
+        <Stack direction="row" spacing={1} sx={{ flexGrow: 1 }}>
+          <Button
+            sx={utilityButtonSx}
+            startIcon={<LanguageOutlinedIcon />}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            English
+          </Button>
+          <Button sx={utilityButtonSx} startIcon={<ToggleOffOutlinedIcon />}>
+            High Contrast
+          </Button>
+          <Button sx={utilityButtonSx} startIcon={<MessageOutlinedIcon />}>
+            Customer Support
+          </Button>
+          <Button
+            sx={utilityButtonSx}
+            startIcon={<PersonOutlineOutlinedIcon />}
+          >
+            Contact Sales
+          </Button>
+        </Stack>
+        <Stack direction="row">
+          <Button startIcon={<SearchOutlinedIcon />}></Button>
+          <Button sx={utilityButtonSx}>Log in</Button>
+          <Button sx={utilityButtonSx} endIcon={<KeyboardArrowDownIcon />}>
+            About{" "}
+          </Button>
+        </Stack>
+      </Toolbar>
+
+      <Toolbar sx={{ mx: 20 }}>
+        <img src={HubSpotLogo} alt="HubSpotLogo" className="h-7 w-auto" />
+        <Stack direction="row" spacing={1} sx={{ flexGrow: 1 }}>
+          <Button endIcon={<KeyboardArrowDownIcon />}>Products</Button>
+          <Button endIcon={<KeyboardArrowDownIcon />}>Solutions</Button>
+          <Button>Pricing</Button>
+          <Button endIcon={<KeyboardArrowDownIcon />}>Resources</Button>
+        </Stack>
+        <Stack>
+          <Button variant="contained" color="primary">
+            Get Free CRM
+          </Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Contacts from './Contacts.jsx';
+import Contacts from "./Contacts.jsx";
+import NavBar from "./NavBar.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#FF7A59", contrastText: "#FFFFFF" }, // CTA orange
+    secondary: { main: "#33475B" }, // slate
+    neutral: { main: "#000000" },
+    text: { primary: "#ff4900", secondary: "#000000"},
+    background: { default: "#FFFFFF", paper: "#FFFFFF" },
+    divider: "#E5E7EB",
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        color: "inherit",
+        variant: "text",
+      },
+    }}
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="contacts" element={<Contacts />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
+);
