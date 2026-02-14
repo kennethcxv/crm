92673f7d978b97aca0a7345bcc398bbd32fd14c4
Kenneth Camacho <Kennethcxv@gmail.com>
2026-02-13 21:38:54 -0500

    Added the form in a seperate page and added logic to recieve and show the data inside the form. And added logic to clear the inputs

--- src/pages/CreateContactListForm.jsx
+++ src/pages/CreateContactListForm.jsx
@@ -8,8 +8,22 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 
+// We make an input state vaiable for each then push it into the array and show it
+
 const CreateContactListForm = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [inputsArr, setInputsArr] = useState([]);
+  const initialInputs = { // we set it to initial inputs so we can clear the inputs after being used
+    email: "",
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    leadSource: "",
+    jobTitle: "",
+    leadStatus: "",
+  };
+
+  const [inputs, setInputs] = useState(initialInputs);
 
   const textStyling = {
     textAlign: "left",
@@ -20,7 +34,15 @@ const CreateContactListForm = () => {
   };
   const TextFieldStyling = {
     backgroundColor: "secondary.secondary",
-    "& .MuiInputBase-input": { color: "black" } // All inputs while typed are colored black
+    "& .MuiInputBase-input": { color: "black" }, // All inputs while typed are colored black
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    setInputsArr([...inputsArr, inputs]);
+    setInputs(initialInputs);
+    console.log(inputsArr);
   };
 
   return (
@@ -67,62 +89,108 @@ const CreateContactListForm = () => {
           </Stack>
 
           <Box sx={{ px: 5 }}>
-            <Stack>
-              <Typography sx={textStyling}>Email *</Typography>
-              <TextField
-                sx={TextFieldStyling}
-                label="Please enter your email"
-                variant="outlined"
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>First Name *</Typography>
-              <TextField
-                label="Please enter your First Name"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>Last Name *</Typography>
-              <TextField
-                label="Please enter your Last Name"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>Phone Number</Typography>
-              <TextField
-                label="Please enter your Phone Number"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>Lead Source *</Typography>
-              <TextField
-                label="Please enter your Lead Source"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>Job title</Typography>
-              <TextField
-                label="Please enter your Job title"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
-            <Stack>
-              <Typography sx={textStyling}>Lead status *</Typography>
-              <TextField
-                label="Please enter your Lead status"
-                variant="outlined"
-                sx={TextFieldStyling}
-              />
-            </Stack>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <Stack>
+                <Typography sx={textStyling}>Email *</Typography>
+                <TextField
+                  sx={TextFieldStyling}
+                  label="Please enter your email"
+                  variant="outlined"
+                  value={inputs.email}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, email: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>First Name *</Typography>
+                <TextField
+                  label="Please enter your First Name"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.firstName}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, firstName: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>Last Name *</Typography>
+                <TextField
+                  label="Please enter your Last Name"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.lastName}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, lastName: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>Phone Number</Typography>
+                <TextField
+                  label="Please enter your Phone Number"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.phoneNumber}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, phoneNumber: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>Lead Source *</Typography>
+                <TextField
+                  label="Please enter your Lead Source"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.leadSource}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, leadSource: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>Job title</Typography>
+                <TextField
+                  label="Please enter your Job title"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.jobTitle}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, jobTitle: e.target.value })
+                  }
+                />
+              </Stack>
+              <Stack>
+                <Typography sx={textStyling}>Lead status *</Typography>
+                <TextField
+                  label="Please enter your Lead status"
+                  variant="outlined"
+                  sx={TextFieldStyling}
+                  value={inputs.leadStatus}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, leadStatus: e.target.value })
+                  }
+                />
+              </Stack>
+              <Button type="submit">Submit</Button>
+            </form>
+            {inputsArr.map((items) => {
+              return (
+                <>
+                  <p>
+                    {items.email}
+                    {items.firstName}
+                    {items.lastName}
+                    {items.phoneNumber}
+                    {items.leadSource}
+                    {items.jobTitle}
+                    {items.leadStatus}
+                  </p>
+                </>
+              );
+            })}
           </Box>
         </Box>
       </Drawer>
